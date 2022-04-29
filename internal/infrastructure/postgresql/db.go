df9825044524cbb932065d3fc43f198dc960ad9d
sokorahen-szk <kamonotakahashi@gmail.com>
2022-04-29 23:32:53 +0900

    #5 infra層 postgresql

---
+++ internal/infrastructure/postgresql/db.go
@@ -0,0 +1,35 @@
+package postgresql
+
+import (
+	"fmt"
+
+	cfg "github.com/sokorahen-szk/rust-live/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PostgreSql struct {
+	db *gorm.DB
+}
+
+func NewPostgreSQL(c *cfg.Config) *PostgreSql {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
+		c.PostgreSql.Host,
+		c.PostgreSql.User,
+		c.PostgreSql.Password,
+		c.PostgreSql.DbName,
+		c.PostgreSql.Port,
+		c.PostgreSql.SslMode,
+		c.PostgreSql.Tz,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return &PostgreSql{
+		db: db,
+	}
+}
