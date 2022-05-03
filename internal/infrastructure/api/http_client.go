908e41e896105b40961ab01bd9b30a0d8ea6d473
sokorahen-szk <kamonotakahashi@gmail.com>
2022-05-03 19:50:36 +0900

    httpClient実装、テストコード追加

--- internal/infrastructure/api/http_client.go
+++ internal/infrastructure/api/http_client.go
@@ -1,17 +1,33 @@
 package api
 
 import (
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"time"
 )
 
+const httpClientTimeout = 5
+
 type HttpClient struct {
-	req *http.Request
+	req    *http.Request
+	params url.Values
 }
 
-type Header struct {
+type RequestHeader struct {
 	key   string
 	value string
+}
+
+type RequestParam struct {
+	key   string
+	value string
+}
+
+type GetJsonResponse struct {
+	v interface{}
 }
 
 func NewHttpClient(method string, url string, body io.Reader) *HttpClient {
@@ -19,13 +35,55 @@ func NewHttpClient(method string, url string, body io.Reader) *HttpClient {
 	if err != nil {
 		panic(err)
 	}
-	return &HttpClient{
+
+	httpClient := &HttpClient{
 		req: req,
+	}
+
+	httpClient.params = req.URL.Query()
+
+	return httpClient
+}
+
+func (r *HttpClient) AddHeaders(headers []RequestHeader) {
+	for _, header := range headers {
+		r.req.Header.Add(header.key, header.value)
 	}
 }
 
-func (r *HttpClient) AddHeader(headers []Header) {
-	for _, header := range headers {
-		r.req.Header.Add(header.key, header.value)
+func (r *HttpClient) AddParams(params []RequestParam) {
+	for _, param := range params {
+		r.params.Add(param.key, param.value)
+	}
+}
+
+func (r *HttpClient) GetJson(v interface{}) (*GetJsonResponse, error) {
+	r.req.URL.RawQuery = r.params.Encode()
+
+	client := r.createClient()
+	httpResponse, err := client.Do(r.req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer httpResponse.Body.Close()
+
+	body, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal([]byte(body), v)
+	if err != nil {
+		return nil, nil
+	}
+
+	response := &GetJsonResponse{v: v}
+	return response, nil
+}
+
+func (r *HttpClient) createClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Duration(httpClientTimeout * time.Second),
 	}
 }
---
+++ internal/infrastructure/api/http_client_test.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestHttpBinOrgGet struct {
+	Args    *TestHttpBinOrgGetParam
+	Url     string `json:"url"`
+	Headers *TestHttpBinOrgGetHeaders
+}
+
+type TestHttpBinOrgGetParam struct {
+	Test string `json:"test"`
+}
+
+type TestHttpBinOrgGetHeaders struct {
+	Hoge string `json:"hoge"`
+	Fuga string `json:"fuga"`
+}
+
+func Test_NewHttpClient_Get(t *testing.T) {
+	a := assert.New(t)
+
+	url := "https://httpbin.org/get"
+
+	t.Run("Getパラメータなし", func(t *testing.T) {
+		client := NewHttpClient(http.MethodGet, url, nil)
+		httpBinOrgGet := new(TestHttpBinOrgGet)
+
+		res, err := client.GetJson(httpBinOrgGet)
+		a.NoError(err)
+		a.NotNil(res)
+
+		actual := res.v.(*TestHttpBinOrgGet)
+		a.Equal(url, actual.Url)
+		a.Equal("", actual.Args.Test)
+		a.Equal("", actual.Headers.Hoge)
+		a.Equal("", actual.Headers.Fuga)
+	})
+	t.Run("Getパラメータあり", func(t *testing.T) {
+		client := NewHttpClient(http.MethodGet, url, nil)
+		httpBinOrgGet := new(TestHttpBinOrgGet)
+
+		client.AddParams([]RequestParam{{"test", "abcd"}})
+		res, err := client.GetJson(httpBinOrgGet)
+		a.NoError(err)
+		a.NotNil(res)
+
+		actual := res.v.(*TestHttpBinOrgGet)
+		a.Equal(fmt.Sprintf("%s?test=abcd", url), actual.Url)
+		a.Equal("abcd", actual.Args.Test)
+		a.Equal("", actual.Headers.Hoge)
+		a.Equal("", actual.Headers.Fuga)
+	})
+	t.Run("Getパラメータあり, ヘッダー追加あり", func(t *testing.T) {
+		client := NewHttpClient(http.MethodGet, url, nil)
+		httpBinOrgGet := new(TestHttpBinOrgGet)
+
+		client.AddParams([]RequestParam{{"test", "abcd"}})
+		client.AddHeaders([]RequestHeader{{"Hoge", "test"}})
+		client.AddHeaders([]RequestHeader{{"Fuga", "test2"}})
+		res, err := client.GetJson(httpBinOrgGet)
+		a.NoError(err)
+		a.NotNil(res)
+
+		actual := res.v.(*TestHttpBinOrgGet)
+		a.Equal(fmt.Sprintf("%s?test=abcd", url), actual.Url)
+		a.Equal("abcd", actual.Args.Test)
+		a.Equal("test", actual.Headers.Hoge)
+		a.Equal("test2", actual.Headers.Fuga)
+	})
+}
