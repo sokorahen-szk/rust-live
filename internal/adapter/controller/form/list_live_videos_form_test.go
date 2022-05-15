39f97a2560b472f072b60a351a902901b34c22fa
sokorahen-szk <kamonotakahashi@gmail.com>
2022-05-15 17:49:11 +0900

    fix list_live_videos_form validator

--- internal/adapter/controller/form/list_live_videos_form_test.go
+++ internal/adapter/controller/form/list_live_videos_form_test.go
@@ -4,32 +4,147 @@ import (
 	"testing"
 
 	pb "github.com/sokorahen-szk/rust-live/api/proto"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_NewListLiveVideosForm(t *testing.T) {
+
+	const (
+		correctSearchKeywords string                        = "example"
+		correctSort           pb.ListLiveVideosRequest_Sort = pb.ListLiveVideosRequest_SORT_UNKNOWN
+		correctPage           int32                         = 1
+		correctLimit          int32                         = 10
+	)
+
 	t.Run("検索キーワード", func(t *testing.T) {
 		tests := []struct {
 			name    string
 			arg     string
 			isError bool
 		}{
+			{"未設定", "", false},
 			{"検索ワード入力", "あいうえおかきくけこさしすせそabcde", false},
-			{"未入力", "", false},
 			{"max=20を超過する", "あいうえおかきくけこさしすせそabcdef", true},
 		}
 		for idx, p := range tests {
 			req := &pb.ListLiveVideosRequest{
 				SearchKeywords: p.arg,
+				Sort:           correctSort,
+				Page:           correctPage,
+				Limit:          correctLimit,
 			}
 
 			t.Run(p.name, func(t *testing.T) {
 				form := NewListLiveVideosForm(req)
 				err := Validate(form)
 				if (err != nil) != p.isError {
-					t.Errorf("pattern %d: want %t, name = %s", idx, p.isError, p.name)
+					t.Errorf("pattern %d: want %t, name = %s, err: %s", idx, p.isError, p.name, err)
 					t.Failed()
 				}
 			})
 		}
+	})
+
+	t.Run("ソートキー", func(t *testing.T) {
+		tests := []struct {
+			name    string
+			arg     pb.ListLiveVideosRequest_Sort
+			isError bool
+		}{
+			{"未設定", pb.ListLiveVideosRequest_SORT_UNKNOWN, false},
+			{"SORT_PLATFORM", pb.ListLiveVideosRequest_SORT_PLATFORM, false},
+			{"SORT_VIEWER_ASC", pb.ListLiveVideosRequest_SORT_VIEWER_ASC, false},
+			{"SORT_VIEWER_DESC", pb.ListLiveVideosRequest_SORT_VIEWER_DESC, false},
+			{"SORT_STARTED_DATETIME_ASC", pb.ListLiveVideosRequest_SORT_STARTED_DATETIME_ASC, false},
+			{"SORT_STARTED_DATETIME_DESC", pb.ListLiveVideosRequest_SORT_STARTED_DATETIME_DESC, false},
+		}
+		for idx, p := range tests {
+			req := &pb.ListLiveVideosRequest{
+				SearchKeywords: correctSearchKeywords,
+				Sort:           p.arg,
+				Page:           correctPage,
+				Limit:          correctLimit,
+			}
+
+			t.Run(p.name, func(t *testing.T) {
+				form := NewListLiveVideosForm(req)
+				err := Validate(form)
+				if (err != nil) != p.isError {
+					t.Errorf("pattern %d: want %t, name = %s, err: %s", idx, p.isError, p.name, err)
+					t.Failed()
+				}
+			})
+		}
+	})
+	t.Run("現在ページ", func(t *testing.T) {
+		tests := []struct {
+			name    string
+			arg     int32
+			isError bool
+		}{
+			{"未設定", 0, true},
+			{"規定値", 1, false},
+		}
+		for idx, p := range tests {
+			req := &pb.ListLiveVideosRequest{
+				SearchKeywords: correctSearchKeywords,
+				Sort:           correctSort,
+				Page:           p.arg,
+				Limit:          correctLimit,
+			}
+
+			t.Run(p.name, func(t *testing.T) {
+				form := NewListLiveVideosForm(req)
+				err := Validate(form)
+				if (err != nil) != p.isError {
+					t.Errorf("pattern %d: want %t, name = %s, err: %s", idx, p.isError, p.name, err)
+					t.Failed()
+				}
+			})
+		}
+	})
+	t.Run("取得する件数", func(t *testing.T) {
+		tests := []struct {
+			name    string
+			arg     int32
+			isError bool
+		}{
+			{"未設定", 0, false},
+			{"規定値", 1, false},
+		}
+		for idx, p := range tests {
+			req := &pb.ListLiveVideosRequest{
+				SearchKeywords: correctSearchKeywords,
+				Sort:           correctSort,
+				Page:           correctPage,
+				Limit:          p.arg,
+			}
+
+			t.Run(p.name, func(t *testing.T) {
+				form := NewListLiveVideosForm(req)
+				err := Validate(form)
+				if (err != nil) != p.isError {
+					t.Errorf("pattern %d: want %t, name = %s, err: %s", idx, p.isError, p.name, err)
+					t.Failed()
+				}
+			})
+		}
+	})
+
+	t.Run("Getter値チェック", func(t *testing.T) {
+		req := &pb.ListLiveVideosRequest{
+			SearchKeywords: correctSearchKeywords,
+			Sort:           correctSort,
+			Page:           correctPage,
+			Limit:          correctLimit,
+		}
+
+		form := NewListLiveVideosForm(req)
+		err := Validate(form)
+		assert.NoError(t, err)
+		assert.Equal(t, correctSearchKeywords, form.GetSearchKeywords())
+		assert.Equal(t, int(correctSort.Number()), form.GetSort())
+		assert.Equal(t, int(correctPage), form.GetPage())
+		assert.Equal(t, int(correctLimit), form.GetLimit())
 	})
 }
--- internal/adapter/controller/form/validator.go
+++ internal/adapter/controller/form/validator.go
@@ -1,12 +1,50 @@
 package form
 
 import (
+	pb "github.com/sokorahen-szk/rust-live/api/proto"
 	"gopkg.in/go-playground/validator.v9"
+)
+
+type formValidator struct{}
+
+const (
+	isListLiveVideoSortValidateName string = "is_list_live_video_sort"
 )
 
 func Validate(form interface{}) error {
 	validate := validator.New()
 
-	err := validate.Struct(form)
+	formValidator := &formValidator{}
+	err := formValidator.registerValidations(validate)
+	if err != nil {
+		return err
+	}
+
+	err = validate.Struct(form)
 	return err
+}
+
+func (fv *formValidator) registerValidations(validate *validator.Validate) error {
+	err := validate.RegisterValidation(isListLiveVideoSortValidateName, fv.isListLiveVideoSort)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fv *formValidator) isListLiveVideoSort(fl validator.FieldLevel) bool {
+	value := fl.Field().Int()
+	switch pb.ListLiveVideosRequest_Sort(value) {
+	case pb.ListLiveVideosRequest_SORT_UNKNOWN,
+		pb.ListLiveVideosRequest_SORT_PLATFORM,
+		pb.ListLiveVideosRequest_SORT_VIEWER_ASC,
+		pb.ListLiveVideosRequest_SORT_VIEWER_DESC,
+		pb.ListLiveVideosRequest_SORT_STARTED_DATETIME_ASC,
+		pb.ListLiveVideosRequest_SORT_STARTED_DATETIME_DESC:
+
+		return true
+	}
+
+	return false
 }
