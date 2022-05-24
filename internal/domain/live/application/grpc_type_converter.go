4f9a1531d0b4b3a84a20f96c0ad49630dc299541
sokorahen-szk <kamonotakahashi@gmail.com>
2022-05-25 00:55:21 +0900

    toGrpc変換 修正

--- internal/domain/live/application/grpc_type_converter.go
+++ internal/domain/live/application/grpc_type_converter.go
@@ -13,13 +13,17 @@ func ToGrpcLiveVideos(liveVideos []*entity.LiveVideo) []*pb.LiveVideo {
 	grpcLiveVideos := make([]*pb.LiveVideo, 0)
 	for _, liveVideo := range liveVideos {
 		grpcLiveVideos = append(grpcLiveVideos, &pb.LiveVideo{
-			Id:              liveVideo.GetId().Int32(),
-			Title:           liveVideo.GetTitle().String(),
-			Stremer:         liveVideo.GetStremer().String(),
-			Viewer:          liveVideo.GetViewer().Int32(),
-			ThumbnailImage:  liveVideo.GetThumbnailImage().String(),
-			StartedDatetime: liveVideo.GetStartedDatetime().RFC3339(),
-			ElapsedTimes:    liveVideo.GetElapsedTimes().Int32(),
+			Id:               liveVideo.GetId().Int32(),
+			BroadcastId:      liveVideo.GetBroadcastId().String(),
+			Title:            liveVideo.GetTitle().String(),
+			Url:              liveVideo.GetUrl().String(),
+			Stremer:          liveVideo.GetStremer().String(),
+			Viewer:           liveVideo.GetViewer().Int32(),
+			VideoPlatform:    pb.VideoPlatform(liveVideo.GetPlatform().Int32()),
+			ThumbnailImage:   liveVideo.GetThumbnailImage().String(),
+			StartedDatetime:  liveVideo.GetStartedDatetime().RFC3339(),
+			ElapsedTimes:     liveVideo.GetElapsedTimes().Int32(),
+			ElapsedTimesText: liveVideo.GetElapsedTimes().Text(),
 		})
 	}
 	return grpcLiveVideos
--- internal/domain/live/application/grpc_type_converter_test.go
+++ internal/domain/live/application/grpc_type_converter_test.go
@@ -4,8 +4,60 @@ import (
 	"testing"
 
 	pb "github.com/sokorahen-szk/rust-live/api/proto"
+	"github.com/sokorahen-szk/rust-live/internal/domain/live/entity"
+	mockEntity "github.com/sokorahen-szk/rust-live/tests/domain/live/entity"
 	"github.com/stretchr/testify/assert"
 )
+
+func Test_ToGrpcLiveVideos(t *testing.T) {
+	a := assert.New(t)
+	t.Run("0件の場合、空配列を返すこと", func(t *testing.T) {
+		emptyLiveVideos := []*entity.LiveVideo{}
+		toGrpcLiveVideos := ToGrpcLiveVideos(emptyLiveVideos)
+		a.IsType([]*pb.LiveVideo{}, toGrpcLiveVideos)
+		a.Len(toGrpcLiveVideos, 0)
+	})
+	t.Run("2件以上の場合、api.LiveVideo配列を返すこと", func(t *testing.T) {
+		liveVideos := []*entity.LiveVideo{
+			mockEntity.NewMockLiveVideo(1),
+			mockEntity.NewMockLiveVideo(2),
+		}
+
+		expectedGrpcLiveVideos := []*pb.LiveVideo{
+			{
+				Id:               liveVideos[0].Id.Int32(),
+				BroadcastId:      liveVideos[0].GetBroadcastId().String(),
+				Title:            liveVideos[0].GetTitle().String(),
+				Url:              liveVideos[0].GetUrl().String(),
+				Stremer:          liveVideos[0].GetStremer().String(),
+				Viewer:           liveVideos[0].GetViewer().Int32(),
+				VideoPlatform:    pb.VideoPlatform(liveVideos[0].GetPlatform().Int32()),
+				ThumbnailImage:   liveVideos[0].GetThumbnailImage().String(),
+				StartedDatetime:  liveVideos[0].GetStartedDatetime().RFC3339(),
+				ElapsedTimes:     liveVideos[0].GetElapsedTimes().Int32(),
+				ElapsedTimesText: liveVideos[0].GetElapsedTimes().Text(),
+			},
+			{
+				Id:               liveVideos[1].Id.Int32(),
+				BroadcastId:      liveVideos[1].GetBroadcastId().String(),
+				Title:            liveVideos[1].GetTitle().String(),
+				Url:              liveVideos[1].GetUrl().String(),
+				Stremer:          liveVideos[1].GetStremer().String(),
+				Viewer:           liveVideos[1].GetViewer().Int32(),
+				VideoPlatform:    pb.VideoPlatform(liveVideos[1].GetPlatform().Int32()),
+				ThumbnailImage:   liveVideos[1].GetThumbnailImage().String(),
+				StartedDatetime:  liveVideos[1].GetStartedDatetime().RFC3339(),
+				ElapsedTimes:     liveVideos[1].GetElapsedTimes().Int32(),
+				ElapsedTimesText: liveVideos[1].GetElapsedTimes().Text(),
+			},
+		}
+
+		toGrpcLiveVideos := ToGrpcLiveVideos(liveVideos)
+		a.IsType([]*pb.LiveVideo{}, toGrpcLiveVideos)
+		a.Equal(expectedGrpcLiveVideos, toGrpcLiveVideos)
+		a.Len(toGrpcLiveVideos, 2)
+	})
+}
 
 func Test_ToGrpcPagination(t *testing.T) {
 	a := assert.New(t)
