d03d88ebfd54b07ec4162a6af92027df6595b58b
sokorahen-szk <kamonotakahashi@gmail.com>
2022-05-25 13:56:27 +0900

    Controller,Usecaseテスト修正

--- internal/domain/live/application/list_live_videos_usecase_test.go
+++ internal/domain/live/application/list_live_videos_usecase_test.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"testing"
 
+	pb "github.com/sokorahen-szk/rust-live/api/proto"
 	cfg "github.com/sokorahen-szk/rust-live/config"
 	"github.com/sokorahen-szk/rust-live/internal/domain/live/entity"
 	"github.com/sokorahen-szk/rust-live/internal/infrastructure/redis"
 	redisLive "github.com/sokorahen-szk/rust-live/internal/infrastructure/redis/live"
 	"github.com/sokorahen-szk/rust-live/internal/usecase/live/list"
+	mockEntity "github.com/sokorahen-szk/rust-live/tests/domain/live/entity"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,7 +18,6 @@ func Test_ListLiveVideosUsecase_Handle(t *testing.T) {
 	a := assert.New(t)
 	ctx := context.Background()
 
-	searchKeywords := "keywords"
 	platforms := []*entity.Platform{}
 	sortKey := entity.NewLiveVideoSortKey(0)
 
@@ -27,11 +28,52 @@ func Test_ListLiveVideosUsecase_Handle(t *testing.T) {
 	usecase := NewListLiveVideosUsecase(liveVideoRepository)
 
 	t.Run("liveVideoが0件の時、空を返すこと", func(t *testing.T) {
-		input := list.NewListLiveVideoInput(searchKeywords, platforms, sortKey, 1, 10)
+		redis.Truncate()
+
+		input := list.NewListLiveVideoInput("", platforms, sortKey, 1, 10)
+
+		expectedPagination := &pb.Pagination{
+			Limit:      10,
+			Page:       1,
+			Prev:       1,
+			Next:       1,
+			TotalPage:  0,
+			TotalCount: 0,
+		}
 
 		actual, err := usecase.Handle(ctx, input)
 		a.NotNil(actual)
 		a.NoError(err)
 		a.Len(actual.LiveVideos, 0)
+		a.Equal(expectedPagination, actual.Pagination)
+	})
+	t.Run("liveVideoが存在する時、LiveVideo配列を返すこと", func(t *testing.T) {
+		redis.Truncate()
+
+		input := list.NewListLiveVideoInput("", platforms, sortKey, 1, 10)
+
+		liveVideos := []*entity.LiveVideo{
+			mockEntity.NewMockLiveVideo(1),
+			mockEntity.NewMockLiveVideo(2),
+			mockEntity.NewMockLiveVideo(3),
+		}
+
+		expectedPagination := &pb.Pagination{
+			Limit:      10,
+			Page:       1,
+			Prev:       1,
+			Next:       1,
+			TotalPage:  1,
+			TotalCount: 3,
+		}
+
+		err := liveVideoRepository.Create(ctx, liveVideos)
+		a.NoError(err)
+
+		actual, err := usecase.Handle(ctx, input)
+		a.NotNil(actual)
+		a.NoError(err)
+		a.Len(actual.LiveVideos, 3)
+		a.Equal(expectedPagination, actual.Pagination)
 	})
 }
