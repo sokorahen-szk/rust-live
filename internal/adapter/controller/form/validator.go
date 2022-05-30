7338dfe1cff8e9aa93fa63975d547bbd6eff40a6
sokorahen-szk <kamonotakahashi@gmail.com>
2022-05-31 01:50:18 +0900

    validatorメッセージカスタム

--- internal/adapter/controller/form/validator.go
+++ internal/adapter/controller/form/validator.go
@@ -21,8 +21,39 @@ func Validate(form interface{}) error {
 		return err
 	}
 
-	err = validate.Struct(form)
+	return formValidator.validateStruct(validate, form)
+}
+
+func (fv *formValidator) validateStruct(validate *validator.Validate, form interface{}) error {
+	err := validate.Struct(form)
+	if err == nil {
+		return nil
+	}
+
+	/*
+		for _, err := range err.(validator.ValidationErrors) {
+			errMessage := fv.validatorFieldMessage(err.Field())
+			// ここでエラーメッセージを新しく配列として作る
+		}
+	*/
 	return err
+}
+
+func (fv *formValidator) validatorFieldMessage(fieldName string) string {
+	switch fieldName {
+	case "SearchKeywords":
+		return "入力された検索キーワードが正しくありません。"
+	case "Platform":
+		return "配信プラットフォームが正しくありません。"
+	case "Sort":
+		return "ソートキーが正しくありません。"
+	case "Page":
+		return "現在ページが正しくありません。"
+	case "Limit":
+		return "取得する件数が正しくありません。"
+	}
+
+	return "validation error"
 }
 
 func (fv *formValidator) registerValidations(validate *validator.Validate) error {
