b4b0978778c5022e4f12588cceb6cc7f3331eb54
yuki_suzuki <46346954+sokorahen-szk@users.noreply.github.com>
2023-02-06 02:07:42 +0900

    Fix/fetch twtich res batch (#43)
    * Fix: ArchiveVideoRepository.List
    
    * Add: ListInput修正

--- internal/infrastructure/postgresql/db.go
+++ internal/infrastructure/postgresql/db.go
@@ -6,11 +6,10 @@ import (
 	cfg "github.com/sokorahen-szk/rust-live/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 type PostgreSql struct {
-	db *gorm.DB
+	*gorm.DB
 }
 
 func NewPostgreSQL(c *cfg.Config) *PostgreSql {
@@ -32,55 +31,13 @@ func NewPostgreSQL(c *cfg.Config) *PostgreSql {
 	}
 
 	return &PostgreSql{
-		db: db,
+		db,
 	}
-}
-
-func (ps *PostgreSql) Create(value interface{}) error {
-	resultTx := ps.db.Debug().Create(value)
-	if resultTx.Error != nil {
-		return resultTx.Error
-	}
-
-	return nil
-}
-
-func (ps *PostgreSql) Get(value interface{}, query interface{}, args ...interface{}) error {
-	resultTx := ps.db.Debug().Where(query, args...).First(value)
-	if resultTx.Error != nil {
-		return resultTx.Error
-	}
-
-	return nil
-}
-
-func (ps *PostgreSql) Update(value interface{}, updateValues interface{}) error {
-	resultTx := ps.db.Model(value).Clauses(clause.Returning{}).Updates(updateValues)
-	if resultTx.Error != nil {
-		return resultTx.Error
-	}
-
-	return nil
-}
-
-func (ps *PostgreSql) List(value interface{}, query interface{}, args ...interface{}) error {
-	var resultTx *gorm.DB
-	if len(query.(string)) == 0 {
-		resultTx = ps.db.Debug().Find(value)
-	} else {
-		resultTx = ps.db.Debug().Where(query, args...).Find(value)
-	}
-
-	if resultTx.Error != nil {
-		return resultTx.Error
-	}
-
-	return nil
 }
 
 func (ps *PostgreSql) Truncate(tables []string) error {
 	for _, table := range tables {
-		resultTx := ps.db.Debug().Exec(fmt.Sprintf("DELETE FROM %s;", table))
+		resultTx := ps.Debug().Exec(fmt.Sprintf("DELETE FROM %s;", table))
 		if resultTx.Error != nil {
 			return resultTx.Error
 		}
